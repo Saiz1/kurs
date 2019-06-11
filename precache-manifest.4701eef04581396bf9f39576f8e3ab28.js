6808b0e79de207d2c5dcf7179222ff682470e734
Saiz1 <saiztw@gmail.com>
2019-06-11 18:15:19 +0300

    Updates

---
+++ precache-manifest.4701eef04581396bf9f39576f8e3ab28.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eae9650fc58eb21a91c432786d9f1f75",
+    "url": "/kurs/static/media/subway.eae9650f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kurs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d817d3a18e437e12e243aa3475b89f53",
+    "url": "/kurs/static/media/Raleway-LightItalic.d817d3a1.ttf"
+  },
+  {
+    "revision": "1f43e4f21a0b08df2629fe68a0b72721",
+    "url": "/kurs/static/media/Raleway-ExtraLight.1f43e4f2.ttf"
+  },
+  {
+    "revision": "3914b181a18d5cad1866",
+    "url": "/kurs/static/js/runtime~main.3914b181.js"
+  },
+  {
+    "revision": "1a2c696b947d8bea8f87",
+    "url": "/kurs/static/js/main.1a2c696b.chunk.js"
+  },
+  {
+    "revision": "8ab05e0372d90f141c43",
+    "url": "/kurs/static/js/2.8ab05e03.chunk.js"
+  },
+  {
+    "revision": "1a2c696b947d8bea8f87",
+    "url": "/kurs/static/css/main.7ce416d0.chunk.css"
+  },
+  {
+    "revision": "bfece330723c110bd7918c653fd4fdfe",
+    "url": "/kurs/index.html"
+  }
+];
